cb8b4ee301b1729dba16a79269abb8c53de57ac3
Yungi <zoshs27@gmail.com>
2020-11-05 01:31:27 +0900

    first commit

--- titanic.py
+++ titanic.py
@@ -1,6 +1,19 @@
-import os
-import pandas as pd
-import numpy as np
+'''
+Overview
+
+This is the original data from Titanic competition plus some changes that I applied to it to be better suited for binary logistic regression:
+
+1. Merged the train and test data.
+2. Removed the 'ticket' and 'cabin' attributes.
+3. Moved the 'Survived' attribute to the last column.
+4. Added extra zero columns for categorical inputs to be better suited for One-Hot-Encoding.
+5. Substituted the values of 'Sex' and 'Embarked' attributes with binary and categorical values respectively.
+6. Filled the missing values in 'Age' and 'Fare' attributes with the median of the data.
+'''
+
+import os # Operating System Control
+import pandas as pd # Data Processing
+import numpy as np # Linear Algebra
 
 # os.system("pwd")
 # /Users/yungi/Documents/Hello_Atom/Titanic
@@ -9,4 +22,17 @@ for dirname, _, filenames in os.walk("/Users/yungi/Documents/Hello_Atom/Titanic/
         print(os.path.join(dirname, filename))
 
 data = pd.read_csv("/Users/yungi/Documents/Hello_Atom/Titanic/inputs/train_and_test2.csv")
-print(data.tail())
+
+## Importing Libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## Modeling Libraries
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier # Study
+from sklearn.ensemble import RandomForestClassifier # Study
+## Evaluate Model
+from sklearn.metrics import confusion_matrix, roc_curve
+
+
